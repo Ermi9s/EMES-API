5d04043ed3a2351fa9ae9b69e7cfd2f42a4ccc64
ermi9s <ermiayele1122334477@gmail.com>
2025-01-21 19:08:27 +0300

    swagger permission

--- EMESapi/EMESapi/urls.py
+++ EMESapi/EMESapi/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 
 schema_view = get_schema_view(
@@ -30,6 +31,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
