e1942fbe5fe07cb0d494ff2c943223390d1bfaae
ermi9s <ermiayele1122334477@gmail.com>
2025-01-22 02:53:11 +0300

    some fixes

--- EMESapi/EMESapi/settings.py
+++ EMESapi/EMESapi/settings.py
@@ -149,6 +149,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'  # Public URL for serving media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
