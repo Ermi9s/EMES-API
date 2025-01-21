448d85e30984bc8f56c16b92275dceef8d9a8fbc
ermi9s <ermiayele1122334477@gmail.com>
2025-01-21 21:16:28 +0300

    swagger done

--- EMESapi/User/urls.py
+++ EMESapi/User/urls.py
@@ -18,16 +18,16 @@ urlpatterns = [
     path('register/' , register , name='register'),
     path('login/', login , name='login'),
 
-    path('users/', UserFetch.as_view(), name='user_list'),
-    path('users/<int:user_id>/', UserFetch.as_view(), name='user_detail'),
-    path('user/<int:user_id>/', UserFetch.as_view(), name='user_fetch'),
-    path('user/update/<str:action>/', UserRegistrationUpdates.as_view(), name='user_update'),
+    path('/users/', UserFetch.as_view(), name='user_list'),
+    path('/users/<int:user_id>/', UserFetch.as_view(), name='user_detail'),
+    path('/user/<int:user_id>/', UserFetch.as_view(), name='user_fetch'),
+    path('/user/update/<str:action>/', UserRegistrationUpdates.as_view(), name='user_update'),
 
-    path('admin/users/', AdminUserView.as_view(), name='admin_user_list'),
-    path('admin/users/<int:user_id>/', AdminUserView.as_view(), name='admin_user_detail'),
-    path('admin/users/status/<str:status_filter>/', AdminUserView.as_view(), name='admin_user_by_status'),
-    path('admin/user/manage/<str:action>/<int:user_id>/', UserManagementView.as_view(), name='user_management'),
-    path('admin/approve-request/<int:request_id>/', AdminUserView.as_view(), name='approve_request'),
+    path('/admin/users/', AdminUserView.as_view(), name='admin_user_list'),
+    path('/admin/users/<int:user_id>/', AdminUserView.as_view(), name='admin_user_detail'),
+    path('/admin/users/status/<str:status_filter>/', AdminUserView.as_view(), name='admin_user_by_status'),
+    path('/admin/user/manage/<str:action>/<int:user_id>/', UserManagementView.as_view(), name='user_management'),
+    path('/admin/approve-request/<int:request_id>/', AdminUserView.as_view(), name='approve_request'),
 
     path('/', include(router.urls)),
 ]
