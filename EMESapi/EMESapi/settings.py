cab6dde4d6ceaeb104ea921f1abca6b4972cbee5
ermi9s <ermiayele1122334477@gmail.com>
2025-01-22 03:13:45 +0300

    some fixes

--- EMESapi/EMESapi/settings.py
+++ EMESapi/EMESapi/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'EMESapi.urls'
@@ -148,13 +149,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'  # Public URL for serving media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
