df2e7c9ea3900f0bfa00af90425e17d0493ea6b0
ermi9s <ermiayele1122334477@gmail.com>
2025-01-22 03:48:04 +0300

    admin registration

--- EMESapi/User/admin.py
+++ EMESapi/User/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Address,
+    Contact,
+    Education,
+    ProfessionalExperience,
+    Publications,
+    Projects,
+    Patents,
+    Award,
+    AnnualMembershipFee,
+    User,
+    ViewRequests,
+)
+
+admin.site.register(Address)
+admin.site.register(Contact)
+admin.site.register(Education)
+admin.site.register(ProfessionalExperience)
+admin.site.register(Publications)
+admin.site.register(Projects)
+admin.site.register(Patents)
+admin.site.register(Award)
+admin.site.register(AnnualMembershipFee)
+admin.site.register(User)
+admin.site.register(ViewRequests)
